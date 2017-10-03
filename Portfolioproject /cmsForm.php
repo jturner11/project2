c561135899966f89de9c184a053981de142cefb6
jturner11 <joshturner11@hotmail.co.uk>
2017-10-03 11:55:41 +0100

    seperated different functions into divs

---
+++ Portfolioproject /cmsForm.php
@@ -0,0 +1,22 @@
+<div class="cmsBackground">
+    <div
+         <a href="addForm.php">ADD DATA</a>
+    </div>
+    <div>
+        <a href="editForm.php">EDIT DATA</a>
+    </div>
+    <div>
+        <a href="deleteForm.php">DELETE DATA</a>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+<?php
+?>
+
