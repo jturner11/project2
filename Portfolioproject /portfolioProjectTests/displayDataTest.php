34fa911ad03e6c427d3b75d61624d954023c2bf8
jturner11 <joshturner11@hotmail.co.uk>
2017-10-04 14:15:08 +0100

    Tests for portfolio project

--- Portfolioproject /portfolioProjectTests/displayDataTest.php
+++ Portfolioproject /portfolioProjectTests/displayDataTest.php
@@ -1,7 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: academy
- * Date: 04/10/2017
- * Time: 13:41
- */
+use PHPUnit\Framework\TestCase;
+require("../displayData.php");
+
+class StackTest extends TestCase {
+
+    public function selectHomepageData(){
+        //        Can't currently test as we have no knowledge on how to carry out tests on the Database
+    }
+    public function selectAboutpageData(){
+        //        Can't currently test as we have no knowledge on how to carry out tests on the Database
+    }
+    public function selectPortfoliopageData(){
+    //        Can't currently test as we have no knowledge on how to carry out tests on the Database
+    }
+}
+?>
