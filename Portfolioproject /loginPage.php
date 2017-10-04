347809c50d9091c78b27869617ecb9b22c6714a3
jturner11 <joshturner11@hotmail.co.uk>
2017-10-04 15:41:55 +0100

    adding code to functions and creating a test page for the login

--- Portfolioproject /loginPage.php
+++ Portfolioproject /loginPage.php
@@ -5,24 +5,25 @@
         $username = "user";
         $password = "password";
         if (isset($_SESSION["logged_in"]) && $_SESSION["logged_in"] == TRUE) {
-            header("location: loginPage.php");
+            header("location: index.php");
         }
         if (isset($_POST["username"]) && isset($_POST["password"])) {
             if ($_POST["username"] == $username && $_POST["password"] == $password)
                 $_SESSION["logged_in"] = TRUE;
-            header("location: loginPage.php");
+            header("location: index.php");
 }   ?>
 </head>
 <body class="loginBody">
     <div class="loginPage">
         <h1>Welcome, please log in to your account</h1>
-
+        <br>
         <form method="post"  action="phpform.php">
-            <h1>Please Log In </h1>
-            <h3> Username: </h3>
+            <h4> Username: </h4>
             <input type="text" name="username">
-            <h3> Password: </h3>
+           <br><br>
+            <h4> Password: </h4>
             <input type="password" name="password">
+            <br>
             <input type="submit" value="login">
         </form>
 
---
+++ Portfolioproject /loginpageFunctionsTest.php
@@ -0,0 +1,18 @@
+<?php
+use PHPUnit\Framework\TestCase;
+require("../functions.php");
+// TESTS FOR LOGGEDIN
+public function testisLoggedInsuccess() {
+}
+public function testisLoggedInfailure() {
+}
+public function testisLoggedInmalformed() {
+}
+//TESTS FOR THE LOG IN FORM
+public function testlogInsuccess(){
+}
+public function testlogInmalfailure(){
+}
+public function testlogInmalformed(){
+}
+?>
