f28db69980b061f47b6bf27e1a64f3c1a3f0aac8
jturner11 <joshturner11@hotmail.co.uk>
2017-10-04 15:30:21 +0100

    login page PHP functions

--- Portfolioproject /loginPage.php
+++ Portfolioproject /loginPage.php
@@ -1,9 +1,30 @@
 <head>
     <link rel="stylesheet" type="text/css" href="index.css">
+    <?php
+        session_start();
+        $username = "user";
+        $password = "password";
+        if (isset($_SESSION["logged_in"]) && $_SESSION["logged_in"] == TRUE) {
+            header("location: loginPage.php");
+        }
+        if (isset($_POST["username"]) && isset($_POST["password"])) {
+            if ($_POST["username"] == $username && $_POST["password"] == $password)
+                $_SESSION["logged_in"] = TRUE;
+            header("location: loginPage.php");
+}   ?>
 </head>
 <body class="loginBody">
     <div class="loginPage">
         <h1>Welcome, please log in to your account</h1>
+
+        <form method="post"  action="phpform.php">
+            <h1>Please Log In </h1>
+            <h3> Username: </h3>
+            <input type="text" name="username">
+            <h3> Password: </h3>
+            <input type="password" name="password">
+            <input type="submit" value="login">
+        </form>
 
     </div>
 </body>
---
+++ Portfolioproject /loginPageFunctions.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * IS LOGGED IN
+ *
+ * @return bool
+ */
+function isLoggedIn() {
+    if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] == false) {
+        return false;
+    }
+    return true;
+}
+/**
+ * THE LOG IN FORM
+ *
+ * @return bool
+ */
+function logIn() {
+    $username = "user";
+    $password = "password";
+    if (isset($_POST["username"]) && isset($_POST["password"])) {
+        if ($_POST["username"] == $username && $_POST["password"] == $password) {
+            return true;
+        }
+    }
+    return false;
+}
