eca05ff91ff757d779e020c0c78ae7dba98fe776
jturner11 <joshturner11@hotmail.co.uk>
2017-10-05 11:19:28 +0100

    make sure form fields are valid input types

--- Portfolioproject /addPortfolioForm.php
+++ Portfolioproject /addPortfolioForm.php
@@ -3,21 +3,22 @@
 <body class="portfolioAddCss">
 <br>
    <h1>Welcome to the add feature.<br>Here you can add new projects easily to your portfolio.</h1>
-    <br>
+    <br><br>
     <h2>PROJECT NAME:</h2>
-   <br><br><br>
+   <br>
 <div class="portfolioAddCss1">
         <input type="text" name="text">
     <br><br>
     <h2>IMAGE</h2>
    <br>
-    <input type="text" name="img">
+    <input type="url" name="img">
     <br><br>
     <h2>URL</h2>
    <br>
-    <input type="text" name="url">
+    <input type="url" name="url">
     <br><br>
 </div>
     <input type="submit" value="Submit">
 </form>
+<br><br><br>
 </body>
